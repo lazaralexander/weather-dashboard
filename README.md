de1e87c7f719465eae64353a05e63bc6d28a1cd3
Alexander Lazar <alexanderjohnlazar@gmail.com>
2022-04-02 16:57:51 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# weather-dashboard
+# Weather Dashboard
+
+## Description
+
+In this project we were tasked with creating a weather dashboard. Using bootstrap, jQuery, and the openweather api we created a simple dashboard to check the weather of your favorite city!
+
+# Assingment Details
+
+## User Story
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+```
+
+# Installation
+
+1. Go to https://github.com/lazaralexander/weather-dashboard
+2. Clone the repository
+3. Open emulation layer for Git command and clone the link
+4. Open in preferred source-code editor
+
+# Usage
+
+![Screenshot of Website](https://github.com/lazaralexander/day-scheduler/blob/main/images/Screenshot%202022-03-26%20100628.png "Website")
+
+# Links
+
+1. https://github.com/lazaralexander/weather-dashboard
+
+# Credits
+
+1. https://github.com/johnfyoung
+2. https://www.w3schools.com/
---
+++ assets/script.js
@@ -0,0 +1,212 @@
+//global variables
+
+const favList = document.getElementById("fav-list");
+const searchEl = document.getElementById("search-btn");
+const userInputEl = document.getElementById("user-input");
+const removeAllBtn = document.getElementById("remove-all-btn");
+
+let cityGeo = {};
+let cityWeather = {};
+let userInput = "";
+
+const weatherIcons = {
+	"01d": "http://openweathermap.org/img/wn/01d.png",
+	"02d": "http://openweathermap.org/img/wn/02d.png",
+	"03d": "http://openweathermap.org/img/wn/02d.png",
+	"04d": "http://openweathermap.org/img/wn/04d.png",
+	"09d": "http://openweathermap.org/img/wn/09d.png",
+	"10d": "http://openweathermap.org/img/wn/10d.png",
+	"11d": "http://openweathermap.org/img/wn/11d.png",
+	"13d": "http://openweathermap.org/img/wn/13d.png",
+	"50d": "http://openweathermap.org/img/wn/50d.png",
+};
+
+async function getCityData() {
+	//One Call API variables
+	const apiRoot = "https://api.openweathermap.org/data/2.5/onecall";
+	const apiQuery = `?lat=${cityGeo.coord.lat}&lon=${cityGeo.coord.lon}&exclude=minutely,hourly,alerts&units=imperial`;
+	const apiKey = "&appid=0da948eb4dff4edc16d53212f5e1d944";
+
+	const fetchForecast = await fetch(apiRoot + apiQuery + apiKey)
+		.then((response) => response.json())
+		.then((data) => {
+			cityWeather = data;
+		});
+}
+
+//grabs city geo location
+async function getCityGeo(city) {
+	const apiRoot = "https://api.openweathermap.org/data/2.5/forecast";
+	const apiQuery = "?cnt=1&appid=0da948eb4dff4edc16d53212f5e1d944&q=";
+
+	const fetchCityGeo = await fetch(apiRoot + apiQuery + city)
+		.then((response) => {
+			if (!response.ok) {
+				alert("Please try again.");
+			} else {
+				return response.json();
+			}
+		})
+		.then((data) => {
+			cityGeo = data.city;
+			getCityData();
+		});
+}
+
+//this function captures the user input
+async function getUserInput() {
+	userInput = userInputEl.value;
+	await getCityGeo(userInput);
+	if (!userInput) {
+		alert("Search query cannot be empty. \n Please try again");
+	} else if (!getCityGeo(userInput)) {
+		alert(`Unable to find ${userInput}. \n Please try again`);
+	} else {
+		addFavEl();
+	}
+}
+
+function addFavEl() {
+	const favEl = document.getElementById("fav-list");
+	const buttonEl = document.createElement("button");
+	buttonEl.classList.add("mt-2", "d-flex-fluid", "btn", "btn-secondary");
+	buttonEl.setAttribute("id", "fav-btn");
+	if (cityGeo) {
+		if (cityGeo.name === localStorage.getItem(cityGeo.name)) {
+			return alert(
+				`${cityGeo.name} is already in your list!`
+			);
+		} else if (Object.keys(localStorage).length > 9) {
+			return alert(`You can only have 10 items in your list.`);
+		}
+		buttonEl.textContent = cityGeo.name;
+		favEl.appendChild(buttonEl);
+		setTimeout(() => {
+			addContent();
+		}, 500);
+	}
+	buttonEl.addEventListener("click", addFavListener);
+}
+
+function addFavListener(event) {
+	const favEl = event.target;
+	const cityName = favEl.textContent;
+	getCityGeo(cityName);
+	setTimeout(() => {
+		addContent(cityName);
+	}, 500);
+}
+
+//forecast data is collected
+function addContent(city = cityGeo.name) {
+	const favEl = document.getElementById("main-section");
+	const currentCity = document.getElementById("current-city");
+	const currentIcon = document.getElementById("current-icon");
+	const currentTemp = document.getElementById("current-temp");
+	const currentWind = document.getElementById("current-wind");
+	const currentHumidity = document.getElementById("current-humidity");
+	const currentUv = document.getElementById("current-uv-p");
+	const cardEl = document.getElementById("cards").children;
+
+	localStorage.setItem(cityGeo.name, cityGeo.name);
+	currentCity.textContent = `${cityGeo.name} ${convertUnixTime(
+		cityWeather.daily[0].dt
+	)}`;
+	currentIcon.setAttribute(
+		"src",
+		weatherIcons[cityWeather.daily[0].weather[0].icon]
+	);
+	currentTemp.innerHTML = "Temp: " + cityWeather.daily[0].temp.day + "&#8457";
+	currentWind.textContent = "Wind: " + cityWeather.daily[0].wind_speed + " MPH";
+	currentHumidity.textContent =
+		"Humidity: " + cityWeather.daily[0].humidity + "%";
+	currentUv.textContent = `UV Index:  `;
+	favEl.classList.remove("invisible");
+	setUvi();
+
+	for (let i = 0; i < cardEl.length; i++) {
+		cardEl[i].children[0].textContent = `${convertUnixTime(
+			cityWeather.daily[i + 1].dt
+		)}`;
+
+		cardEl[i].children[1].setAttribute(
+			"src",
+			weatherIcons[cityWeather.daily[i + 1].weather[0].icon]
+		);
+		cardEl[i].children[2].innerHTML =
+			"Temp: " + cityWeather.daily[i + 1].temp.day + "&#8457";
+		cardEl[i].children[3].textContent =
+			"Wind: " + cityWeather.daily[i + 1].wind_speed + " MPH";
+		cardEl[i].children[4].textContent =
+			"Humidity: " + cityWeather.daily[i + 1].humidity + "%";
+	}
+}
+
+function convertUnixTime(timestamp) {
+	const timeStamp = parseInt(timestamp) * 1000;
+	const date = new Date(timeStamp);
+	const dateFormat = date.toLocaleDateString();
+	return dateFormat;
+}
+
+function setUvi() {
+	const uviEl = document.getElementById("current-uv-span");
+	const uviValue = parseInt(cityWeather.daily[0].uvi);
+	if (uviValue <= 2) {
+		uviEl.setAttribute("class", "uv-low");
+		uviEl.textContent = uviValue;
+	} else if (uviValue <= 5) {
+		uviEl.setAttribute("class", "uv-moderate");
+		uviEl.textContent = uviValue;
+	} else if (uviValue <= 7) {
+		uviEl.setAttribute("class", "uv-high");
+		uviEl.textContent = uviValue;
+	} else {
+		uviEl.setAttribute("class", "uv-very-high");
+		uviEl.textContent = uviValue;
+	}
+}
+
+function renderStorageEl() {
+	const favEl = document.getElementById("fav-list");
+
+	cityEl = Object.keys(localStorage);
+	if (cityEl.length > 0) {
+		for (let i = 0; i < cityEl.length; i++) {
+			const buttonEl = document.createElement("button");
+			buttonEl.classList.add("mt-2", "d-flex-fluid", "btn", "btn-secondary");
+			buttonEl.setAttribute("id", "fav-btn");
+			buttonEl.textContent = cityEl[i];
+			favEl.appendChild(buttonEl);
+		}
+		async function getUserClick() {
+			const btnEl = document.querySelectorAll("#fav-btn");
+			for (let i = 0; i < btnEl.length; i++) {
+				btnEl[i].addEventListener("click", () => {
+					getCityGeo(btnEl[i].textContent);
+					setTimeout(() => {
+						addContent();
+					}, 500);
+				});
+			}
+		}
+		getUserClick();
+	}
+}
+
+function removeAllEl() {
+	const btnEl = document.querySelectorAll("#fav-btn");
+	const cityEl = Object.keys(localStorage);
+	for (let i = 0; i < cityEl.length; i++) {
+		localStorage.removeItem(cityEl[i]);
+		btnEl[i].remove();
+	}
+}
+
+function initApp() {
+	searchEl.addEventListener("click", getUserInput);
+	removeAllBtn.addEventListener("click", removeAllEl);
+	renderStorageEl();
+}
+
+initApp();
